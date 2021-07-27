d677da51b37506d36c296f7a10f01c8d120cb53c
YoonMinSeok <81290520+YoonMinSeok@users.noreply.github.com>
2021-07-27 17:18:06 +0900

    20210727_2
    realtime update

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,35 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(()=>{
     authService.onAuthStateChanged((user)=> {
       if(user){
-        setIsLoggedIn(true);
-        setUserObj(user);
-      }else{
-        setIsLoggedIn(false);
+        setUserObj({
+          displayName: user.displayName,
+          uid : user.id,
+          updateProfile : (args) => user.updateProfile(args),
+        });
       }
       setInit(true);
     });
-  });
+  }, []);
+  const refreshUser = ()=>{
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid : user.id,
+      updateProfile : (args) => user.updateProfile(args),
+    });
+  }
   return (
     <>
       {init? (
-        <AppRouter isLoggedIn={Boolean(userObj)} userObj = {userObj}/>
+        <AppRouter 
+         refreshUser = {refreshUser}
+         isLoggedIn={Boolean(userObj)} 
+         userObj = {userObj}
+        />
       ) : (
         "Initializing.." 
       )}
